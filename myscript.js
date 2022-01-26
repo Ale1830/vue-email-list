01933c2b7f615e326f12b9a6f40cc6c40295aab9
Alessio Tiberi <alessio.1997.18.01@gmail.com>
2022-01-26 03:31:34 +0100

    fatto

---
+++ myscript.js
@@ -0,0 +1,32 @@
+const app = new Vue({
+    el: "#root",
+    
+    data :{
+        arrayEmail:[],
+    },
+    
+
+    methods: {
+        carica: function () {
+          axios
+            .get("https://flynn.boolean.careers/exercises/api/random/mail")
+            .then((risposta) => {
+
+              console.log(risposta.data.response);
+              this.arrayEmail.push(risposta.data.response);
+             
+
+             
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        },
+      },
+      mounted() {
+        for(let i=0; i<10; i++){
+            this.carica()
+        }
+      
+    }
+})
